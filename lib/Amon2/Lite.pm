9a7a58cd5804ec33a7bac560aaab1a2ad7531c4f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-14 09:24:16 +0900

    initial import

---
+++ lib/Amon2/Lite.pm
@@ -0,0 +1,39 @@
+package Amon2::Lite;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Lite -
+
+=head1 SYNOPSIS
+
+  use Amon2::Lite;
+
+=head1 DESCRIPTION
+
+Amon2::Lite is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
